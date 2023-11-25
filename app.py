2bf53de310e3476e938687426b1847b6779fdf20
tasoskele <tasoskelemain@gmail.com>
2023-11-25 22:13:34 +0200

    ver 2 - added bootstrap/created db/some frontend

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
+from "" import _sqlite3
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+db = SQL("sqlite:///persons.db")
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+#Delete person
+
+#Validate submission
